ddfb477f1294fa210c5642c06c750a58e952b609
nurmancell <nurmancell@gmail.com>
2016-11-27 23:22:23 +0700

    Create js1.js

---
+++ js1.js
@@ -0,0 +1,20 @@
+<script type='text/javascript'>
+//<![CDATA[
+$(document).ready(function(){ 
+ var touch  = $('#resp-menu');
+ var menu  = $('.menu');
+ 
+ $(touch).on('click', function(e) {
+  e.preventDefault();
+  menu.slideToggle();
+ });
+ 
+ $(window).resize(function(){
+  var w = $(window).width();
+
+  if(w > 767 && menu.is(':hidden')) {
+   menu.removeAttr('style');
+  }
+ });
+ 
+  });//]]></script>
